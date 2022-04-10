9678a9dbff87d5b7f212be23372876c0a13fdf59
Aayush Gupta <riser.aayush@gmail.com>
2022-04-10 17:34:02 +0530

    6th Login Page

--- app.js
+++ app.js
@@ -75,6 +75,11 @@ console.log(result);
 res.redirect("/stage2/ques");
 });
 
+app.get("/login",(req,res)=>{
+res.sendFile(__dirname+"/views/login.html");
+});
+
+
 app.listen(3000,async()=> {
     console.log("Server started on port 3000.");
     await sequelize.authenticate();
